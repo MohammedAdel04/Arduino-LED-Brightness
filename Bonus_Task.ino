e08b52da35f677b1e607102258f59bd6950f9c65
MohammedAdel04 <158074476+MohammedAdel04@users.noreply.github.com>
2024-02-05 18:12:43 +0200

    Bonus_Task.ino

--- Bonus_Task.ino
+++ Bonus_Task.ino
@@ -1,6 +1,5 @@
 int amount =0;
 const int led = 9;
-int x=0;
 void setup()
 {
   pinMode(led, OUTPUT);
@@ -8,26 +7,22 @@ void setup()
 
 void loop()
 {
-  
+
   if(amount<255)
-  {
-    x=x+5;
-    amount =x+5;
+  {;
+    amount =amount+5;
     analogWrite(led,amount);
     delay(50);
   }
   else if(amount>=255)
   {
     amount = 0;
-    x=0;
     while(amount<255)
     {
-      x=x+5;
-    amount =x+5;
+    amount =amount+5;
     analogWrite(led,255-amount);
     delay(50);
     }
     amount =0;
-    x=0;
   }
 }
